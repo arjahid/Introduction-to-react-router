4bbaab0915994b113d64ffb485d9af143ccb5398
Jahid-Hasan <jhjahidhasanar@gmail.com>
2025-02-03 21:25:39 +0600

    fix main jsx

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -9,6 +9,8 @@ const Header = () => {
                 <Link to="/">Home</Link>
                 <Link to="/about">About</Link>
                 <Link to="/contact">Contact</Link>
+                <Link to="/users">Users</Link>
+                
                 <Link to="/blog">Blog</Link>
             </nav>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,35 @@
+
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import About from "./Components/About/About";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./Components/Home/Home";
+import About from "./Components/About/About";
 import Contact from "./Components/Contact/Contact";
 import Blog from "./Components/Blog/Blog";
+import Users from "./Components/Users/Users";
 
+// Loader Function for Users
+// const userLoader = async () => {
+//   const res = await fetch("https://jsonplaceholder.typicode.com/users");
+//   if (!res.ok) {
+//     throw new Error("Failed to fetch users");
+//   }
+//   return res.json();
+// };
 
-const root = document.getElementById("root");
+// Router Setup
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    children: [
+      { path: "about", element: <About /> },
+      { path: "contact", element: <Contact /> },
+      { path: "blog", element: <Blog /> },
+      { path: "users", element: <Users />, loader:()=>fetch("https://jsonplaceholder.typicode.com/users")  },
+    ],
+  },
+]);
 
-ReactDOM.createRoot(root).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />}>
-        <Route path="about" element={<About />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="blog" element={<Blog />} />
-      
-      </Route>
-    </Routes>
-  </BrowserRouter>
-); 
-
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
